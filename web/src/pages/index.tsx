06f4eaa268b22fca1a88e3956b623ea82931af6d
23yimichael <23yimichael@gmail.com>
2023-01-24 02:26:27 -0800

    nextjs + tailwindcss setup

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>{`health{hacks}`}</title>
+        <meta
+          name="description"
+          content="Assembling the future innovators of medicine."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className=""></div>
+    </>
+  );
+}
