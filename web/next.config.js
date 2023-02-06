9a9ff81dc3e0bd7e22fee1e873a9b3a51ee596e5
michaelhyi <michaelyi2004@gmail.com>
2023-02-06 06:16:04 -0800

    fileloader test

--- web/next.config.js
+++ web/next.config.js
@@ -17,3 +17,24 @@ module.exports = {
 };
 
 module.exports = nextConfig;
+
+export default {
+  webpack(config, { isServer }) {
+    const prefix = config.assetPrefix ?? config.basePath ?? "";
+    config.module.rules.push({
+      test: /\.mp4$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            publicPath: `${prefix}/_next/static/media/`,
+            outputPath: `${isServer ? "../" : ""}static/media/`,
+            name: "[name].[hash].[ext]",
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
