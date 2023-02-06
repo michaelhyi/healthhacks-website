4760e8aee7871fc82ec07f8f9b72c942cf0418d7
michaelhyi <michaelyi2004@gmail.com>
2023-02-06 06:08:40 -0800

    test commit again

--- web/components/Day.tsx
+++ web/components/Day.tsx
@@ -10,12 +10,14 @@ const Day: React.FC<Props> = ({ id, title, desc }) => {
   return (
     <div className="flex sm:flex-col sm:space-y-24 sm:space-x-0 xl:sm:flex-row xl:space-y-0 xl:space-x-48 items-center">
       <video
+        playsInline
         autoPlay
-        // loop
+        loop
         muted
         className="sm:w-[320px] xl:w-[384px] 2xl:w-[512px]"
-        src={`/days/Day ${id}.mp4`}
-      />
+      >
+        <source src={`/days/Day ${id}.mp4`} type="video/mp4" />
+      </video>
       <div className="flex flex-col sm:text-center sm:w-[384px] md:w-[512px] xl:text-left xl:w-[384px] 2xl:w-[512px] space-y-4">
         <div className="font-semibold text-3xl">{`Day ${id}: ${title}`}</div>
         <div className="font-medium opacity-75 text-xl xl:w-[384px] 2xl:w-[512px]">
