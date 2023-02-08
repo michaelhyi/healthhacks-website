124f14719e81a9f92977ae9f452060e7e8c6c3d3
michaelhyi <michaelyi2004@gmail.com>
2023-02-08 09:47:32 -0800

    code cleanup, logos bugs fixed

--- web/components/Day.tsx
+++ web/components/Day.tsx
@@ -4,23 +4,11 @@ interface Props {
   id: number;
   title: string;
   desc: string;
-  embed: string;
 }
 
-const Day: React.FC<Props> = ({ id, title, desc, embed }) => {
+const Day: React.FC<Props> = ({ id, title, desc }) => {
   return (
     <div className="flex sm:flex-col sm:space-y-16 sm:space-x-0 xl:sm:flex-row xl:space-y-0 xl:space-x-48 items-center">
-      {/* <div className="pointer-events-none">
-        <iframe
-          width="662"
-          height="1176"
-          className="sm:w-[320px] xl:w-[384px] 2xl:w-[512px]"
-          src={`https://www.youtube.com/embed/${embed}?autoplay=1&mute=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&playlist=${embed}`}
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        ></iframe>
-      </div> */}
-
       <video
         playsInline
         autoPlay
--- web/data/days.ts
+++ web/data/days.ts
@@ -3,18 +3,15 @@ export const days = [
     id: 1,
     title: "Experience the power of collaboration.",
     desc: "Meet and brainstorm with a diverse group of creators and listen to inspiring keynote speeches. Connect with others and share ideas to make a positive impact in the healthcare industry.",
-    embed: "kGYf46ZBWVM",
   },
   {
     id: 2,
     title: "Enhance your knowledge and skills.",
     desc: "Continue to work on your projects and listen to informative workshops led by industry experts. Learn and grow in the healthcare industry and take your skills to the next level.",
-    embed: "xF4PxOWmUY0",
   },
   {
     id: 3,
     title: "Pitch your ideas to the world.",
     desc: "Pitch your projects to a panel of judges and have the chance to win prizes. Showcase your ideas and make a difference in the world of healthcare.",
-    embed: "gdDSzyPl3Xw",
   },
 ];
