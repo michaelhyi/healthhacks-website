4b8807e113820884e5b2c39cead4acc7fd7204bf
michaelhyi <michaelyi2004@gmail.com>
2023-02-09 21:34:55 -0800

    hidden pages added to dev branch

---
+++ web/pages/blog.tsx
@@ -0,0 +1,14 @@
+import Container from "../components/Container";
+
+const Blog = () => {
+  return (
+    <Container>
+      <div className="w-[75vw] mx-auto">
+        <div className="font-semibold text-5xl mt-24">Blog</div>
+        <div className="w-full bg-[#d9d9d9f7] h-[50vh] rounded-3xl mt-12"></div>
+      </div>
+    </Container>
+  );
+};
+
+export default Blog;
