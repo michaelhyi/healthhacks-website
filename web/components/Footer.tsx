1a03656d403508fda6e00c68aeb96aa970195760
michaelhyi <michaelyi2004@gmail.com>
2023-02-22 09:41:48 -0800

    updated env variables

--- web/components/Footer.tsx
+++ web/components/Footer.tsx
@@ -13,12 +13,11 @@ const Footer = () => {
   const toast = useToast();
 
   // Config variables
-  const SPREADSHEET_ID = process.env.NEXT_PUBLIC_MAILING_LIST_SPREADSHEET_ID;
-  const SHEET_ID = process.env.NEXT_PUBLIC_MAILING_LIST_SHEET_ID;
-  const GOOGLE_CLIENT_EMAIL =
-    process.env.NEXT_PUBLIC_MAILING_LIST_GOOGLE_CLIENT_EMAIL;
+  const SPREADSHEET_ID = process.env.NEXT_PUBLIC_SPREADSHEET_ID;
+  const SHEET_ID = process.env.NEXT_PUBLIC_SHEET_ID;
+  const GOOGLE_CLIENT_EMAIL = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL;
   const GOOGLE_SERVICE_PRIVATE_KEY =
-    process.env.NEXT_PUBLIC_MAILING_LIST_GOOGLE_SERVICE_PRIVATE_KEY;
+    process.env.NEXT_PUBLIC_GOOGLE_SERVICE_PRIVATE_KEY;
 
   // GoogleSpreadsheet Initialize
   const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
