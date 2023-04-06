a7be65165f4f610355c7da1cd17508a28bf86c2d
William Pan <willpan@stanford.edu>
2023-04-06 15:18:31 -0700

    Refined countdown

---
+++ web/components/dashboard/home/EventHelp.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+const EventHelp = () => {
+
+  return (
+    <div className="flex flex-col justify-center items-center bg-hh-gray border-opacity-50 w-full rounded-2xl p-8">
+      <p className="text-xl font-medium text-center">
+        Help:
+      </p>
+    </div>
+  );
+};
+
+export default EventHelp;
--- web/pages/dashboard/index.tsx
+++ web/pages/dashboard/index.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import NavbarContainer from "../../components/dashboard/NavbarContainer";
 import ContainerApp from "../../components/ContainerApp";
 import Footer from "../../components/dashboard/FooterDash";
-import EventCountdown from "../../components/dashboard/EventCountdown";
+import EventCountdown from "../../components/dashboard/home/EventCountdown";
+import EventHelp from "../../components/dashboard/home/EventHelp";
 import TitleDash from "../../components/dashboard/TitleDash";
 
 export default function Dashboard() {
@@ -28,12 +29,16 @@ export default function Dashboard() {
     <NavbarContainer>
       <div className="m-16">
         <TitleDash title="Participant Dashboard" />
-        <div className="flex flex-col md:flex-row h-[100vh]">
+        <div className="flex flex-col md:flex-row h-[50vh]">
           <div className="w-1/2"></div>
-          <div className="w-1/2 h-1/2">
-            <EventCountdown />
+          <div className="flex flex-col w-1/2 gap-y-4">
+            <div className="">
+              <EventCountdown />
+            </div>
+            <div className="h-1/2">
+              <EventHelp />
+            </div>
           </div>
-
         </div>
       </div>
       <Footer />
