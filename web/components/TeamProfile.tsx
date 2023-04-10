470791ff7f9d4d8d59449689ce52900efdcd7bfc
michaelhyi <michaelyi2004@gmail.com>
2023-04-10 10:02:35 -0700

    team formation functionalities implemented

--- web/components/TeamProfile.tsx
+++ web/components/TeamProfile.tsx
@@ -1,19 +1,27 @@
 import React from "react";
+import { BsFillPersonFill } from "react-icons/bs";
 
 interface Props {
-  firstName: string;
-  lastName: string;
-  email: string;
+  user: {
+    firstName: string;
+    lastName: string;
+    email: string;
+  };
 }
 
-const TeamProfile: React.FC<Props> = ({ firstName, lastName, email }) => {
+const TeamProfile: React.FC<Props> = ({ user }) => {
   return (
-    <div className="rounded-2xl border-2 border-dashed border-gray-400">
-      <div className="text-xl">
-        {firstName}&nbsp;
-        {lastName}
+    <div className="rounded-2xl border-2 border-dashed border-gray-400 p-4 w-[512px]">
+      <div className="flex space-x-4">
+        <BsFillPersonFill size={100} />
+        <div>
+          <div className="text-3xl font-bold">
+            {user.firstName}&nbsp;
+            {user.lastName}
+          </div>
+          <div>{user.email}</div>
+        </div>
       </div>
-      <div>{email}</div>
     </div>
   );
 };
--- web/pages/dashboard/team-formation.tsx
+++ web/pages/dashboard/team-formation.tsx
@@ -1,14 +1,28 @@
-import React, { useEffect, useState } from "react";
-import ProfileGrid from "../../components/ProfileGrid";
-import NavbarContainer from "../../components/dashboard/NavbarContainer";
+import * as EmailValidator from "email-validator";
+import { GoogleSpreadsheetRow } from "google-spreadsheet";
 import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 import ContainerApp from "../../components/ContainerApp";
-import { readParticipants } from "../../utils/helpers";
 import TeamProfile from "../../components/TeamProfile";
+import NavbarContainer from "../../components/dashboard/NavbarContainer";
+import { readParticipants } from "../../utils/helpers";
+import { useToast } from "@chakra-ui/react";
 
 const App: React.FC = () => {
+  const toast = useToast();
   const router = useRouter();
+  const [profiles, setProfiles] = useState<
+    | null
+    | {
+        firstName: string;
+        lastName: string;
+        email: string;
+      }[]
+  >(null);
+  const [data, setData] = useState<null | GoogleSpreadsheetRow[]>(null);
   const [fetching, setFetching] = useState<boolean>(true);
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
 
   useEffect(() => {
     (async () => {
@@ -16,18 +30,87 @@ const App: React.FC = () => {
       if (!response) {
         router.push("/login");
       } else {
-        // const user = JSON.parse(response);
-        const user = {
-          email: "adamzhao@stanford.edu",
-          track: "Aging",
-        };
+        const user = JSON.parse(response);
+        setProfiles([user]);
         const rows = await readParticipants();
         const track = rows!.find((v) => v._rawData[0] === user.email);
-        const data = rows!.filter((v) => v._rawData[3] === track!._rawData[3]);
+        setData(rows!.filter((v) => v._rawData[3] === track!._rawData[3]));
         setFetching(false);
       }
     })();
   }, []);
+
+  const handleQuery = async () => {
+    if (profiles!.length === 4) {
+      toast({
+        title: "Error!",
+        description: "You cannot add more than 4 participants to your team!",
+        status: "error",
+        duration: 10000,
+        isClosable: true,
+      });
+      return;
+    }
+    if (email.length === 0) {
+      toast({
+        title: "Error!",
+        description: "You must enter an email!",
+        status: "error",
+        duration: 10000,
+        isClosable: true,
+      });
+      return;
+    }
+
+    const user = data!.find((v) => v._rawData[0] === email);
+
+    if (!EmailValidator.validate(email) || !user) {
+      setError(
+        "Email is invalid, does not exist, or the participant is under a different track."
+      );
+      toast({
+        title: "Error!",
+        description:
+          "Email is invalid, does not exist, or the participant is under a different track.",
+        status: "error",
+        duration: 10000,
+        isClosable: true,
+      });
+      return;
+    }
+
+    if (profiles?.find((v) => v.email === user._rawData[0])) {
+      toast({
+        title: "Error!",
+        description: "You already added this participant.",
+        status: "error",
+        duration: 10000,
+        isClosable: true,
+      });
+      return;
+    }
+
+    setProfiles([
+      ...profiles!,
+      {
+        firstName: user._rawData[1],
+        lastName: user._rawData[2],
+        email: user._rawData[0],
+      },
+    ]);
+  };
+
+  const handleSubmit = () => {
+    if (profiles!.length < 2) {
+      toast({
+        title: "Error!",
+        description: "You must create a team of 2-4 participants.",
+        status: "error",
+        duration: 10000,
+        isClosable: true,
+      });
+    }
+  };
 
   if (fetching) {
     return (
@@ -39,13 +122,35 @@ const App: React.FC = () => {
 
   return (
     <NavbarContainer>
-      <div className="grid grid-cols-2 gap-12">
-        <TeamProfile
-          firstName="Michael"
-          lastName="Yi"
-          email="michaelyi2004@gmail.com"
-        />
+      <div className="grid grid-cols-2 gap-12 p-12">
+        {profiles?.map((v) => (
+          <TeamProfile user={v} />
+        ))}
       </div>
+      <div>
+        <input
+          className="bg-black border-white border-[1px] rounded-lg mr-4"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button
+          onClick={handleQuery}
+          className="hover:cursor-pointer duration-500 hover:opacity-50"
+        >
+          Submit
+        </button>
+        {error && error.length > 0 && (
+          <div className="mt-4 font-poppins font-semibold text-red-400 text-sm">
+            {error}
+          </div>
+        )}
+      </div>
+      <button
+        onClick={handleSubmit}
+        className="mt-48 bg-white text-black p-12 rounded-xl"
+      >
+        SUBMIT TEAM
+      </button>
     </NavbarContainer>
   );
 };
