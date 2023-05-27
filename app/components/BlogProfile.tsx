6ac24de9ee40a58eca8e1745a14518d2da21eb01
michaelhyi <michaelyi2004@gmail.com>
2023-05-27 10:51:12 -0700

    all main frontend pages complete

---
+++ app/components/BlogProfile.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+interface Props {
+  author: string;
+  affiliation: string;
+  pfp: string;
+}
+
+const BlogProfile: React.FC<Props> = ({ author, affiliation, pfp }) => {
+  return (
+    <div className="flex flex-row items-center gap-x-2 mt-4">
+      <div className="w-[30px] h-[30px]">
+        <img src={pfp} alt={author} className="rounded-full" />
+      </div>
+      <div className="leading-tight text-xs font-thin text-[#555]">
+        <b className="font-medium">{author}</b>
+        <br /> {affiliation}
+      </div>
+    </div>
+  );
+};
+
+export default BlogProfile;
